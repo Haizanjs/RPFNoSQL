c70337563430bcba6bb45e50a97ae6059ed4a53a
Jack <jackmichaelsimons59@gmail.com>
2018-03-05 21:34:22 +0000

    Initial Project Commit :+1:

---
+++ RPFNoSQL/DLLEntry.cs
@@ -0,0 +1,20 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using static RPFNoSQL_HandleInput_ClassContainer;
+
+namespace RPFNoSQL
+{
+    public class DLLEntry
+    {
+        [DllExport("RVExtension", CallingConvention = CallingConvention.Winapi)]
+        public static void RVExtension(StringBuilder output, int outputSize, [MarshalAs(UnmanagedType.LPStr)] string function)
+        {
+            output.Append(RPFNoSQL_HandleInput.InputRequest(function));
+        }
+    }
+}
---
+++ RPFNoSQL/HandleInput.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class RPFNoSQL_HandleInput_ClassContainer
+{
+    public partial class RPFNoSQL_HandleInput : RPFNoSQL_HandleInput_ClassContainer
+    {
+        public static char split_character = ':';
+        public static string InputRequest(string inputString)
+        {
+            string returnMessage;
+            string[] splitArgs = inputString.Split(split_character);
+            if (splitArgs.Length < 2)
+            {
+                returnMessage = "RPFNoSQL >> Error >> Invalid Arugments";
+            }
+            else
+            {
+                switch (splitArgs[0].ToLower())
+                {
+                    case "downloadurl":
+                        returnMessage = "";
+                        break;
+                    default:
+                        returnMessage = "RPFNoSQL >> Error >> No arguments passed";
+                        break;
+                }
+            }
+            return (returnMessage);
+            // return result here
+        }
+
+    }
+}
+
